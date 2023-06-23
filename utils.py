ce70a80e6cdd6031b96836394783200c41ba4ed3
fbjarkes <fbjarkes-github.q5706@aleeas.com>
2023-06-23 22:29:32 +0200

    Add Pipfile and utils

---
+++ utils.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import json
+
+
+def parse_json_to_dataframe(symbol: str, path: str) -> pd.DataFrame:
+    file_path = f"{path}/{symbol}.json"
+    
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+        
+    records = data[symbol]
+    
+    df_data = []
+    for record in records:
+        dt = pd.to_datetime(record['DateTime'])
+        #dt = dt.tz_localize('UTC').tz_convert('America/New_York')
+        df_data.append([
+            dt,
+            record['Open'],
+            record['High'],
+            record['Low'],
+            record['Close'],
+            record['Volume']
+        ])
+    
+    df = pd.DataFrame(df_data, columns=['DateTime', 'Open', 'High', 'Low', 'Close', 'Volume'])
+    df.set_index('DateTime', inplace=True)
+    df.index = pd.DatetimeIndex(df.index)
+    df.symbol = symbol
+    
+    return df
