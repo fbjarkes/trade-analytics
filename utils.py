7cccc4dc8444647dabe997c50e5ce68e65178696
fbjarkes <fbjarkes-github.q5706@aleeas.com>
2023-06-23 23:41:41 +0200

    Utils parse csv function

--- utils.py
+++ utils.py
@@ -2,6 +2,12 @@ import pandas as pd
 import json
 
 
+def filter_dates(start: str, end: str, df: pd.DataFrame) -> pd.DataFrame:
+    if start and end:
+        df = df.loc[start:end]
+    return df
+    
+
 def parse_json_to_dataframe(symbol: str, path: str) -> pd.DataFrame:
     file_path = f"{path}/{symbol}.json"
     
@@ -29,3 +35,14 @@ def parse_json_to_dataframe(symbol: str, path: str) -> pd.DataFrame:
     df.symbol = symbol
     
     return df
+
+def parse_csv_to_dataframe(symbol: str, path) -> pd.DataFrame:
+    file_path = f"{path}/{symbol}.csv"
+    data = pd.read_csv(file_path)
+    
+    # Parse time column as DateTimeIndex
+    data['time'] = pd.to_datetime(data['time'], unit='s')
+    data.set_index('time', inplace=True)   
+    data.name = symbol
+    
+    return data
