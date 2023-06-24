7ecaa0c8aac5696479d4caa6a7049a122b30ca80
fbjarkes <fbjarkes-github.q5706@aleeas.com>
2023-06-24 11:48:34 +0200

    Metric functions WIP

--- metric.py
+++ metric.py
@@ -3,10 +3,47 @@ import numpy as np
 import scipy.stats as stats
 from finta import TA
 
-def ema_cloud_10_20(trades: pd.DataFrame, index: pd.DataFrame):
-    merged_df = pd.merge_asof(trades, index['EMA_10_20_Cloud'], left_index=True, right_on='time', direction='nearest')
-    pos_trades = merged_df.loc[merged_df['EMA_10_20_Cloud'] == True, 'pnl']
-    neg_trades = merged_df.loc[merged_df['EMA_10_20_Cloud'] == False, 'pnl']
+
+def ema_cross_10_20(df: pd.DataFrame, metric: str) -> pd.DataFrame:
+    df['EMA10'] = TA.EMA(df, 10)
+    df['EMA20'] = TA.EMA(df, 20)
+    df[metric] = df['EMA10'] >= df['EMA20']
+    return df
+
+def close_ema_5(df: pd.DataFrame, metric: str) -> pd.DataFrame:
+    df['EMA5'] = TA.EMA(df, 5)
+    df[metric] = df['close'] >= df['EMA5']
+    return df
+
+def close_ema_10(df: pd.DataFrame, metric: str) -> pd.DataFrame:
+    df['EMA10'] = TA.EMA(df, 10)
+    df[metric] = df['close'] >= df['EMA10']
+    return df
+
+def close_ema_20(df: pd.DataFrame, metric: str) -> pd.DataFrame:
+    df['EMA20'] = TA.EMA(df, 20)
+    df[metric] = df['close'] >= df['EMA20']
+    return df
+
+def close_ema_50(df: pd.DataFrame, metric: str) -> pd.DataFrame:
+    df['EMA50'] = TA.EMA(df, 50)
+    df[metric] = df['close'] >= df['EMA50']
+    return df
+    
+metrics_functions = {
+    'EMA_CROSS_10_20': ema_cross_10_20,
+    'CLOSE_EMA_5': close_ema_5,
+    'CLOSE_EMA_10': close_ema_10,
+    'CLOSE_EMA_20': close_ema_20,
+    'CLOSE_EMA_50':  close_ema_50
+}
+
+def index_analysis(metric: str, index: pd.DataFrame, trades: pd.DataFrame):
+    func = metrics_functions[metric]
+    func(df=index, metric=metric)
+    merged_df = pd.merge_asof(trades, index[metric], left_index=True, right_on='time', direction='nearest')
+    pos_trades = merged_df.loc[merged_df[metric] == True, 'pnl']
+    neg_trades = merged_df.loc[merged_df[metric] == False, 'pnl']
     pos_t_statistic, pos_p_value = stats.ttest_ind(trades['pnl'], pos_trades)
     neg_t_statistic, neg_p_value = stats.ttest_ind(trades['pnl'], neg_trades)
     return {
@@ -19,29 +56,6 @@ def ema_cloud_10_20(trades: pd.DataFrame, index: pd.DataFrame):
         'neg_t_statistic': neg_t_statistic,
         'neg_p_value': neg_p_value
     }
-
-def ema_cross_5():
-    pass
-
-def ema_cross_10():
-    pass
-
-def ema_cross_20():
-    pass
-
-def ema_cross_50():
-    pass    
-
-metrics_functions = {
-    'EMA_CLOUD_10_20': ema_cloud_10_20,
-    'EMA5_CROSS_5': ema_cross_5,
-    'EMA5_CROSS_10': ema_cross_10,
-    'EMA5_CROSS_20': ema_cross_20,
-    'EMA5_CROSS_50': ema_cross_50 
-}
-
-def index_analysis(metric: str, index: pd.DataFrame, trades: pd.DataFrame):
-    func = metrics_functions[metric]
-    return func(trades, index)
+    
 
     
