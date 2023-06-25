fb995f45a5b6eab0ecc26868f374b79eb35a3993
fbjarkes <fbjarkes-github.q5706@aleeas.com>
2023-06-25 08:58:46 +0200

    Load tickers using async and threads

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
-from typing import List
+from typing import List, Optional
 import pandas as pd
 import json
+from concurrent.futures import ThreadPoolExecutor
+import asyncio
 
 def compose(f, g):
     return lambda *args, **kwargs: f(g(*args, **kwargs))
@@ -58,18 +60,18 @@ def parse_json_to_dataframe(symbol: str, path: str) -> pd.DataFrame:
     
     return df
 
-def json_to_df(symbol: str, path: str) -> pd.DataFrame:  
-    print(f"Reading json file: {path}")  
-    with open(path) as f:
-        json_data = json.load(f)
-        if json_data[symbol]:
-            df = pd.DataFrame(json_data[symbol], columns=['DateTime', 'Open', 'High', 'Low', 'Close', 'Volume'])
-            df.set_index('DateTime', inplace=True)
-            df.index = pd.DatetimeIndex(df.index) # TODO: add freq?
-            df.name = symbol
-            return df
-        else:
-            print(f"Symbol {symbol} not found in json '{path}'")
+# def json_to_df(symbol: str, path: str) -> pd.DataFrame:  
+#     print(f"Reading json file: {path}")  
+#     with open(path) as f:
+#         json_data = json.load(f)
+#         if json_data[symbol]:
+#             df = pd.DataFrame(json_data[symbol], columns=['DateTime', 'Open', 'High', 'Low', 'Close', 'Volume'])
+#             df.set_index('DateTime', inplace=True)
+#             df.index = pd.DatetimeIndex(df.index) # TODO: add freq?
+#             df.name = symbol
+#             return df
+#         else:
+#             print(f"Symbol {symbol} not found in json '{path}'")
 
 def parse_csv_to_dataframe(symbol: str, path) -> pd.DataFrame:
     file_path = f"{path}/{symbol}.csv"
@@ -88,3 +90,42 @@ def load_tickers(path: str, tickers=List[str]) -> List[pd.DataFrame]:
         df = json_to_df(symbol=ticker, path=f"{path}/{ticker}.json")
         data.append(df)
     return data
+
+
+def load_json_data(symbol: str, path: str) -> Optional[dict]:
+    with open(path) as f:
+        json_data = json.load(f)
+        return json_data.get(symbol)
+
+def process_json_data(data: dict, symbol: str) -> pd.DataFrame:
+    if data:
+        df = pd.DataFrame(data, columns=['DateTime', 'Open', 'High', 'Low', 'Close', 'Volume'])
+        df.set_index('DateTime', inplace=True)
+        df.index = pd.DatetimeIndex(df.index)  # TODO: add freq?
+        df.name = symbol
+        return df
+    else:
+        print(f"Symbol {symbol} not found in the json")
+
+def json_to_df(symbol: str, path: str) -> Optional[pd.DataFrame]:
+    data = load_json_data(symbol, path)
+    if data is not None:
+        return process_json_data(data, symbol)
+
+    return None
+
+def load_multiple_json_to_df(root_path: str, symbols: list[str]) -> list[Optional[pd.DataFrame]]:
+    async def process_file(symbol, path):
+        print(f"Processing file: {path}")
+        loop = asyncio.get_running_loop()
+        data = await loop.run_in_executor(None, load_json_data, symbol, path)
+        return process_json_data(data, symbol)
+
+    async def load_files():
+        tasks = []
+        for symbol in symbols:
+            task = asyncio.create_task(process_file(symbol, f"{root_path}/{symbol}.json"))
+            tasks.append(task)
+        return await asyncio.gather(*tasks)
+
+    return asyncio.run(load_files())
